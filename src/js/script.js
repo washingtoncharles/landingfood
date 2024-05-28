525537f33b1c0fe7dea121de95357046a0f65be8
Washington Charles <toncharles@live.com>
2024-05-27 23:35:14 -0300

    feat: add functions header

--- src/js/script.js
+++ src/js/script.js
@@ -3,4 +3,39 @@ $(document).ready(function () {
     $("#mobile-menu").toggleClass("active");
     $("#mobile-btn").find('i').toggleClass("fa-x");
   });
+
+  //SELECT SECTION ITEM
+  const sections = $('section');
+  const navItems = $('.nav-item');
+
+  //FUNCTION TO SELECT SCROLL
+  $(window).on('scroll', function () {
+    const header = $('header');
+    const scrollPosition = $(window).scrollTop() - header.outerHeight(); //FIND SCROLL POSITION
+
+    //REMOVE OR ADD SHADOW FROM THE NAVIGATION BAR
+    if(scrollPosition <= 0) {
+      header.css('box-shadow', 'none');
+    } else {
+      header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1)');
+    }
+
+    //MENU ITEM HIGHLIGHT
+    let activeSectionIndex = 0;
+
+    sections.each(function(i) {
+      const section = $(this);
+      const sectionTop = section.offset().top - 96; //FIND SECTION TOP
+      const sectionBottom = sectionTop + section.outerHeight(); //FIND SECTION BOTTOM
+
+      //CONDITIONAL FOR CHANGING THE INDEX
+      if(scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+        activeSectionIndex = i;
+        return false;
+      }
+    })
+    //REMOVE OR ADD ACTIVE CLASS
+    $(navItems[activeSectionIndex]).addClass('active').siblings().removeClass('active');
+
+  })
 })
